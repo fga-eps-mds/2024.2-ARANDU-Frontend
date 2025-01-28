6a91ff8fe48145756c39fcd93b2421028f26bf72
dualUbuntuLevi <leviqueiroz647@gmail.com>
2025-01-28 00:02:32 -0300

    feat(fga-eps-mds/2024.2-ARANDU-DOC#66): alterado nome e botão e adicionado uma fonte

--- src/components/ui/typography/calculustNavbar.typography.tsx
+++ src/components/ui/typography/calculustNavbar.typography.tsx
@@ -4,16 +4,17 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
+import { amarante } from '../fonts/fonts';
 
 const CalculusHeader: React.FC = () => {
   const { data: session } = useSession();
   return (
     <Typography
-      variant="h6"
+      variant="h4"
       component="div"
       sx={{ flexGrow: 1, color: 'black', fontWeight: 'bold' }}
     >
-      <Link href={session ? '/home' : '/'}>Calculus</Link>
+      <Link href={session ? '/home' : '/'} className={`${amarante.className}`}>ARANDU</Link>
     </Typography>
   );
 };
